8b63b50d85bb9ed7f84cfeeb5bf0bc528e8e25e6
Alexisvib <alexis.vib@gmail.com>
2021-03-17 19:50:17 +0100

    Carroussel feature 2nd Try

--- carroussel.php
+++ carroussel.php
@@ -1,5 +1,14 @@
 <div class="container-caroussel">
     <div class="carroussel-bloc-around"> </div>
-    <div class="carroussel-bloc"> </div>
+    <div class="carroussel-bloc">
+    <figure>
+        <img src="assets/shop.png" alt="Image du shop">
+        <img src="assets/marimba.png" alt="Image de marimba">
+        <img src="assets/rayon-guitar.png" alt="le rayon guitar du shop">
+        <img src="assets/piano.png" alt="Image d'un piano">
+        <img src="assets/guitar.png" alt="Image d'un guitariste'">
+    </figure>
+
+    </div>
     <div class="carroussel-bloc-around"> </div>
 </div>
