c0e7c49e02132497796da28a98f2f971b04cbcbe
P-ROGER06 <proger06@gmail.com>
2021-03-17 18:52:03 +0100

    modif media queries

--- header.php
+++ header.php
@@ -1,20 +1,3 @@
-<!DOCTYPE html>
-<html lang="fr">
-  <head>
-  <link rel="preconnect" href="https://fonts.gstatic.com">
-  <link href="https://fonts.googleapis.com/css2?family=Raleway:wght@600&display=swap" rel="stylesheet"> 
-    <meta charset="utf-8" />
-    <meta name="viewport" content="width=device-width" />
-    <link href="styles.css" rel="stylesheet" type="text/css" />
-
-    <link
-      rel="shortcut icon"
-      type="image/png"
-      href="https://i.ibb.co/vL5WMrb/shortcuticon.png"
-    />
-
-    
-  </head>
 
   <header>
     <div class="header">
@@ -44,5 +27,3 @@
   </ul>
 
   </header>
-  <script type="text/javascript" src="index.js"></script>
-</html>
