37da8bc1dcbda468c1979ea9c5c5d11085529256
A-david1 <antoine.david.pro1@gmail.com>
2021-03-17 12:16:59 +0100

    add assets for homepage + ini files

--- index.php
+++ index.php
@@ -1,1 +1,25 @@
-<?php
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <title>Accueil - Au Son Vert</title>
+    <link rel="stylesheet" href="//fonts.googleapis.com/css?family=font1|font2|etc" type="text/css">
+    <link rel="stylesheet" href="styles.css" type="text/css">
+</head>
+
+<div class="main-container">
+    <div class="carousel">
+        <img src="">
+        
+    </div>
+    
+    
+    
+    
+    
+</div>
+
+<body>
+<h1>hello</h1>
+</body>
+</html>
