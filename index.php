d440e0b365bdbfe4a10aea14e9f87e13ff7fb923
A-david1 <antoine.david.pro1@gmail.com>
2021-03-19 09:06:26 +0100

    add content in index.php

--- index.php
+++ index.php
@@ -1,25 +1,86 @@
 <!DOCTYPE html>
-<html>
+<html lang="fr">
 <head>
     <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width,initial-scale=1">
     <title>Accueil - Au Son Vert</title>
-    <link rel="stylesheet" href="//fonts.googleapis.com/css?family=font1|font2|etc" type="text/css">
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css2?family=Raleway:ital,wght@0,600;1,400&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="styles.css" type="text/css">
+    <meta name="description" content="description here">
+    <meta name="keywords" content="keywords,here">
 </head>
+
+<body>
 
 <div class="main-container">
     <div class="carousel">
-        <img src="">
-        
+        carousel
     </div>
-    
+
+    <div class="selection-products">
+        <div><h2 class="title-h2">Les Nouveautés</h2></div>
+        <div class="container">
+            <div class="card buybox1">
+                    <div class="card-img"></div>
+                    <div class="card-body">
+                        <p class="card-title">Violon Stradivarius</p>
+                        <p class="card-subtitle">Modèle nimbus 2000</p>
+                        <p class="card-price">259,99€</p>
+                        <button class="card-button">Acheter</button>
+                    </div>
+            </div>
+
+            <div class="card buybox2">
+                    <div class="card-img"></div>
+                <div class="card-body">
+                    <p class="card-title">Violon Stradivarius</p>
+                    <p class="card-subtitle">Modèle nimbus 2000</p>
+                    <p class="card-price">77,99€/jour</p>
+                    <button class="card-button button-rend">Louer</button>
+                </div>
+            </div>
+
+            <div class="card buybox3">
+                    <div class="card-img"></div>
+                <div class="card-body">
+                    <p class="card-title">Violon Stradivarius</p>
+                    <p class="card-subtitle">Modèle nimbus 2000</p>
+                    <p class="card-price">259,99€</p>
+                    <button class="card-button">Acheter</button>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="testimony">
+        <div><h2 class="title-h2">Témoignages</h2></div>
+
+        <div class="container">
+
+            <div class="testimonycard">
+                    <p class="card-text">Lodo porta a sit amet nulla. Ut dapibus accumsan erat, vitae imperdiet neque. Donec mi lorem, efficitur ut erat vitae, posuere interdum nisl. Aliquam non lacinia nisl, a dapibus diam.</p>
+                    <p class="card-name">Anna</p>
+            </div>
+
+            <div class="testimonycard">
+                <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus congue scelerisque varius. Aliquam non lacinia nisl, a dapibus diam.</p>
+                <p class="card-name">Guillaume</p>
+            </div>
+
+            <div class="testimonycard">
+                <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit.isl, a dapibus diam.</p>
+                <p class="card-name">Quentin</p>
+            </div>
+
+        </div>
+    </div>
     
     
     
     
 </div>
 
-<body>
-<h1>hello</h1>
+
 </body>
 </html>
