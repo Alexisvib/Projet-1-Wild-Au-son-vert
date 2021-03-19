8da4511c94732bec8be0758aa0d3bfae1dbdab88
Alexisvib <alexis.vib@gmail.com>
2021-03-19 08:18:42 +0100

    correction des liens

--- header.php
+++ header.php
@@ -9,7 +9,7 @@
       <ul id="menusimple">
      <li><a href="index.php">ACCUEIL</a></li>
      <li><a href="shop.php">BOUTIQUE</a></li>
-     <li><a href="#">EVENEMENTS</a></li>
+     <li><a href="event.php">EVENEMENTS</a></li>
      <li><a href="#">L'EQUIPE</a></li>
      <li><a href="form.php">CONTACT</a></li>
   </ul>
@@ -21,7 +21,7 @@
   <ul id="menuBurger">
      <li><a href="index.php">ACCUEIL</a></li>
      <li><a href="shop.php">BOUTIQUE</a></li>
-     <li><a href="#">EVENEMENTS</a></li>
+     <li><a href="event.php">EVENEMENTS</a></li>
      <li><a href="#">L'EQUIPE</a></li>
      <li><a href="form.php">CONTACT</a></li>
   </ul>
