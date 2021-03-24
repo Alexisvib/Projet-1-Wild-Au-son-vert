1ee4106b553451727767412f3e72e0a11372f6ab
P-ROGER06 <proger06@gmail.com>
2021-03-24 16:32:30 +0100

    footer modifications

--- footer.php
+++ footer.php
@@ -1,53 +1,43 @@
-    <div id="scrollUp">
-        <a href="#espace-header"><img src="assets/arrow-circle-up.png" alt="Flèche pour remonter en haut de page" class="arrowUp"></a>
-    </div>
+<div id="scrollUp">
+  <a href="#espace-header"><img src="assets/arrow-circle-up.png" alt="Flèche pour remonter en haut de page" class="arrowUp"></a>
+</div>
 
-    <footer class="footer">
-  <div class="espace-footer"></div>
+<footer class="footer">
 
- 
-  <div class=footer-box>
+  <hr class="hr-footer">
 
-
+  <div class="box-footer">
     <menu class="box-menu">
-      <ul class="menu-footer-text-left">
-        <li class="li-footer"><a href="index.php">ACCUEIL</a></li>
-        <li class="li-footer"><a href="shop.php">BOUTIQUE</a></li>
-        <li class="li-footer"><a href="event.php">EVENEMENTS</a></li>
-      </ul>
-
-      <ul class="menu-footer-text-right">
-        <li class="li-footer"><a href="team.php">L'EQUIPE</a></li>
-        <li class="li-footer"><a href="form.php">CONTACT</a></li>
-      </ul>
+      <a href="index.php" class="footer-link">ACCUEIL</a>
+      <a href="shop.php" class="footer-link">BOUTIQUE</a>
+      <a href="event.php" class="footer-link">EVENEMENTS</a>
+      <a href="team.php" class="footer-link">L'EQUIPE</a>
+      <a href="form.php" class="footer-link">CONTACT</a>
     </menu>
-
     <div class="informations">
-      <ul class="ul-footer">
-        <li class="li-footer"><a>Tél: 07 75 81 64 56</a></li>
-        <li class="li-footer"><a>Mail: au.son.vert@aol.io</a></li>
-        <li class="li-footer"><a>Adresse:</a></li>
-        <li class="li-footer"><a>7 rue du magicobus</a></li>
-        <li class="li-footer"><a>33777 Poudlard</a></li>
-      </ul>
+      <a>
+        Tél: 07 75 81 64 56
+        <br>
+        Mail: au.son.vert@aol.io
+        <br>
+        Adresse:
+        7 rue du magicobus
+        33777 Poudlard
+      </a>
     </div>
-
-  <div class="logo-icone-box">
-    <div class="icone-box">
-      <a href="https://i.ibb.co/H7gZr8d/inico.png">
-        <img src="https://i.ibb.co/tXpm4Ht/fico.png" alt="facebook-icone" class="icone-footer" /></a>
-      <a href="https://i.ibb.co/H7gZr8d/inico.png">
-        <img src="https://i.ibb.co/bBqWHfz/twico.png" alt="tweeter-icone" class="icone-footer" /></a>
-
-      <a href="https://i.ibb.co/H7gZr8d/inico.png">
-        <img src="https://i.ibb.co/H7gZr8d/inico.png" alt="instagram-icone" class="icone-footer" /></a>
+    <div class="logo-icone-box">
+      <div class="icone-box">
+        <a href="https://fr-fr.facebook.com/">
+          <img src="https://i.ibb.co/tXpm4Ht/fico.png" alt="facebook-icone" class="icone-footer" /></a>
+        <a href="https://twitter.com/login?lang=fr">
+          <img src="https://i.ibb.co/bBqWHfz/twico.png" alt="tweeter-icone" class="icone-footer" /></a>
+        <a href="https://www.instagram.com/?hl=fr">
+          <img src="https://i.ibb.co/H7gZr8d/inico.png" alt="instagram-icone" class="icone-footer" /></a>
+      </div>
+      <a href="index.php">
+        <img src="https://i.ibb.co/1MQPrBx/Logotype-texte.png" alt="Logotype-texte" class="Logotype-texte-footer" /></a>
     </div>
-
-    <a href="index.php">
-      <img src="https://i.ibb.co/1MQPrBx/Logotype-texte.png" alt="Logotype-texte" class="Logotype-texte-footer" /></a>
   </div>
-  </div>
-      <script type="text/javascript" src="buttonUp.js"></script>
+  <script type="text/javascript" src="buttonUp.js"></script>
 </footer>
-
 </body>
