1ca42d0c17132edcbf9becf1748885a3d5b088bc
P-ROGER06 <proger06@gmail.com>
2021-03-25 11:24:45 +0100

    footer modifications

--- footer.php
+++ footer.php
@@ -14,6 +14,7 @@
       <a href="team.php" class="footer-link">L'EQUIPE</a>
       <a href="form.php" class="footer-link">CONTACT</a>
     </menu>
+    
     <div class="informations">
       <a>
         Tél: 07 75 81 64 56
@@ -21,23 +22,26 @@
         Mail: au.son.vert@aol.io
         <br>
         Adresse:
+        <br>
         7 rue du magicobus
+        <br>
         33777 Poudlard
       </a>
     </div>
     <div class="logo-icone-box">
       <div class="icone-box">
-        <a href="https://fr-fr.facebook.com/">
+        <a href="https://fr-fr.facebook.com/" target="_blank">
           <img src="https://i.ibb.co/tXpm4Ht/fico.png" alt="facebook-icone" class="icone-footer" /></a>
-        <a href="https://twitter.com/login?lang=fr">
+        <a href="https://twitter.com/login?lang=fr" target="_blank">
           <img src="https://i.ibb.co/bBqWHfz/twico.png" alt="tweeter-icone" class="icone-footer" /></a>
-        <a href="https://www.instagram.com/?hl=fr">
+        <a href="https://www.instagram.com/?hl=fr" target="_blank">
           <img src="https://i.ibb.co/H7gZr8d/inico.png" alt="instagram-icone" class="icone-footer" /></a>
       </div>
       <a href="index.php">
         <img src="https://i.ibb.co/1MQPrBx/Logotype-texte.png" alt="Logotype-texte" class="Logotype-texte-footer" /></a>
     </div>
   </div>
+  <a class="footer-made-text"> made with love by team</a><a class="suite" href="napak.php"> !NAPAK</a>
   <script type="text/javascript" src="buttonUp.js"></script>
 </footer>
 </body>
